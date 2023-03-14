8ce0c5f34cd12839670c63eacf9ee196e0232621
Quentin LAURENT <quentin.laurent@ie-concept.fr>
2023-03-14 09:31:46 +0100

    :whale: Corrected Docker build

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-$nbTableFound = $(MYSQL_PWD=$DATABASE_PASSWORD mysql -u $DATABASE_USER -h $DATABASE_HOST $DATABASE_NAME -e "SELECT COUNT(TABLE_NAME) FROM information_schema.TABLES WHERE TABLE_NAME = 'Todo';")
+nbTableFound=$(mysql -u$DATABASE_USER -h$DATABASE_HOST -p$DATABASE_PASSWORD $DATABASE_NAME -s -N -e "SELECT COUNT(TABLE_NAME) FROM information_schema.TABLES WHERE TABLE_NAME = 'todo';")
+
+while [ $? -ne 0 ]; do
+    echo "Waiting for database to be ready..."
+    sleep 1
+    nbTableFound=$(mysql -u$DATABASE_USER -h$DATABASE_HOST -p$DATABASE_PASSWORD $DATABASE_NAME -s -N -e "SELECT COUNT(TABLE_NAME) FROM information_schema.TABLES WHERE TABLE_NAME = 'todo';")
+done
 
 if [ $nbTableFound -eq 0 ]; then
-    MYSQL_PWD=$DATABASE_PASSWORD mysql -u $DATABASE_USER -h $DATABASE_HOST $DATABASE_NAME -e "CREATE TABLE Todo (id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255), done BOOLEAN);"
+    mysql -u$DATABASE_USER -h$DATABASE_HOST -p$DATABASE_PASSWORD $DATABASE_NAME -e "CREATE TABLE todo (id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255), done BOOLEAN);"
 fi
 
 apache2-foreground
